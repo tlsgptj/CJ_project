52e9fafd3688eabded528af7e7ec237102bc87b3
tlsgptj <angele0709@naver.com>
2024-06-24 01:28:56 +0900

    git commit

--- app/src/main/java/com/example/cj_project/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cj_project/presentation/MainActivity.kt
@@ -1,9 +1,3 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
- * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
- * changes to the libraries and their usages.
- */
-
 package com.example.cj_project.presentation
 
 import android.os.Bundle
@@ -11,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -30,11 +30,8 @@ import com.example.cj_project.presentation.theme.CJ_projectTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
-
         setTheme(android.R.style.Theme_DeviceDefault)
-
         setContent {
             WearApp("Android")
         }
@@ -58,16 +55,32 @@ fun WearApp(greetingName: String) {
 
 @Composable
 fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
+    var heartRate by remember { mutableStateOf(0) }
+    var isHeartRateMeasuring by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (!isHeartRateMeasuring) {
+            Text(
+                text = stringResource(R.string.start_measurement),
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Center
+            )
+        } else {
+            Text(
+                text = "$heartRate bpm",
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp("Wear OS")
 }
